fed49636bab6e8dffb058cc4ce548226ba9f70a6
Nico Swiatecki <nico.swiatecki@gmail.com>
2017-08-28 08:57:16 +0200

    Add new AvatarModule

--- avatar.ts
+++ avatar.ts
@@ -1,1 +1,21 @@
-export * from './src/avatar/avatar.component';
+import { NgModule } from '@angular/core';
+import { AvatarModule } from "./src/avatar/avatar.module";
+
+export {
+  AvatarComponent,
+  AvatarModule
+} from './src/avatar'
+
+const MODULES = [
+  AvatarModule
+];
+
+@NgModule({
+  imports: [
+    AvatarModule
+  ],
+  exports: MODULES
+})
+
+export class AvatarModules {
+}
---
+++ src/avatar/avatar.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AvatarComponent } from "./avatar.component";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    AvatarComponent
+  ],
+  exports: [
+    AvatarComponent
+  ]
+})
+export class AvatarModule { }
