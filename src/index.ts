154766e7829f931f3b03b930b98c737fa4fb2439
Nico Swiatecki <nico.swiatecki@gmail.com>
2017-08-29 00:54:14 +0200

    a

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar.component';
 
+
 export * from './avatar.component'
 
 @NgModule({
