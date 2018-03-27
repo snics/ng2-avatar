e88419b77663a39ca1e0e8f7d132a7826bd791c0
anthonynahas <anthony.na@hotmail.de>
2018-03-27 20:35:20 +0200

    Fix: removed dist dir from project

--- dist/module/avatar.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-export { AvatarComponent } from './component/avatar.component';
-export declare class AvatarModule {
-    static forRoot(): ModuleWithProviders;
-}
